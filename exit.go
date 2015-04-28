3bde05f8cc68c76fef1e272eb055ea839b7780a9
Danyel Bayraktar <danyel@webhippie.de>
2015-04-28 16:19:50 +0200

    Added exit.go with implementation

---
+++ exit.go
@@ -0,0 +1,54 @@
+package exit
+
+import "os"
+
+type hooks []func()
+
+func (h *hooks) Register(f func()) {
+	*h = append(*h, f)
+}
+
+// Whether the exit should recover from panics
+var Recover = true
+
+// The status code the client will terminate with
+// (when no panic occurred.) Can be set any time.
+var Status = 0
+
+// The status code the client will terminate with
+// when a panic has occurred. Can be set any time.
+var StatusOnPanic = 2
+
+// Hooks to be run before exiting.
+var Hooks = make(hooks, 0)
+
+// Whether hooks should be run when no panic has occurred.
+// Can be set any time.
+var RunHooks = true
+
+// Whether hooks should be run on panic. Can be set any time.
+var RunHooksOnPanic = true
+
+// A hook that will be run if a panic has occurred.
+// The value passed to panic will be passed here.
+var PanicHook = func(i interface{}) {}
+
+func lastBreath() {
+	if Recover {
+		if r := recover(); r != nil {
+			RunHooks = RunHooksOnPanic
+			Status = StatusOnPanic
+			PanicHook(r)
+		}
+	}
+	if RunHooks {
+		for _, hook := range Hooks {
+			hook()
+		}
+	}
+}
+
+func Exit() {
+	lastBreath()
+	os.Exit(Status)
+}
